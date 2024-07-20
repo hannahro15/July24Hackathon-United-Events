4ce3229f3ac543e4bd5104e1cf03bf74832534fd
Gary Sorohan <garysorohan@gmail.com>
2024-07-20 17:05:56 +0000

    add date widgets to Add Event Form

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from datetime import date
 from .models import Event
+
+
+class DateInput(forms.DateInput):
+    """
+    Allows the user to choose a date from a calender
+    in the Add Event Form.
+    """
+    input_type = "date"
 
 
 class EventForm(forms.ModelForm):
@@ -11,6 +20,8 @@ class EventForm(forms.ModelForm):
         fields = ('name', 'start_date', 'end_date', 'location',
                   'description', 'highlights', 'accessibility',
                   'ticket_info', 'additional_notes', 'website')
+        widgets = {"start_date": DateInput(),
+                   "end_date": DateInput()}
         labels = {
             'start_date': 'Start Date',
             'end_date': 'End Date',
