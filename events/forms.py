577a40eaedfb085e33f68b78b14bd74515326dd7
Tim Nelson <traveltimn@gmail.com>
2024-07-20 21:17:29 +0200

    Add datepicker to admin event form

--- events/forms.py
+++ events/forms.py
@@ -2,12 +2,20 @@ from django import forms
 from .models import Event
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
 class EventForm(forms.ModelForm):
     """
     The form to allow admin users to add events.
     """
     class Meta:
         model = Event
+        widgets = {
+            "start_date": DateInput(),
+            "end_date": DateInput(),
+        }
         fields = (
             'name', 'start_date', 'end_date', 'location',
             'description', 'highlights', 'accessibility',
