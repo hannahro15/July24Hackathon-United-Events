98b1f839ddf9d8abad70fe1fa8b0fd3235e8f68f
Gary Sorohan <garysorohan@gmail.com>
2024-07-20 13:18:33 +0000

    update events urls

--- events/urls.py
+++ events/urls.py
@@ -2,12 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-<<<<<<< Updated upstream
     path("", views.events, name="events"),
+    path('add_event/', add_event, name='add_event'),
     path("like/<int:event_id>/", views.like_event, name="like-event"),
     path("api/", views.event_list, name="event-list"),
-=======
-    path('', views.events, name='events'),
-    path('add_event/', add_event, name='add_event'),
->>>>>>> Stashed changes
 ]
