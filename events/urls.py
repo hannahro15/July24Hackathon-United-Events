2de552168c17695d7888047697ace70dfd83867c
Tim Nelson <traveltimn@gmail.com>
2024-07-21 15:51:45 +0200

    Update admin-only CRUD management of Events

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.events, name="events"),
     path('add_event/', views.add_event, name='add-event'),
+    path('manage/', views.manage_events, name='manage-events'),
     path('delete_event/<int:event_id>/', views.delete_event, name='delete-event'),
     path('edit_event/<int:event_id>/', views.edit_event, name='edit-event'),
     path("like/<int:event_id>/", views.like_event, name="like-event"),
